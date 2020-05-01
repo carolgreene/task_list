42614ec815fea3cd36898affcfdb8e2031fc0d7c
carolgreene <carolgreene3@gmail.com>
2020-05-01 16:27:18 -0700

    basic js. Tasks add to list

---
+++ app.js
@@ -0,0 +1,41 @@
+//define UI vars
+const form = document.querySelector('#task-form')
+const taskList = document.querySelector('.collection')
+const clearBtn = document.querySelector('.clear-tasks')
+const filter = document.querySelector('#filter')
+const taskInput = document.querySelector('#task')
+
+
+loadEventListeners()
+
+
+function loadEventListeners() {
+  form.addEventListener('submit', addTask)
+    
+}
+
+
+
+function addTask(e) {
+  e.preventDefault()
+  if(taskInput.value === '') {
+    alert('Add a task')
+  }
+  
+  const li = document.createElement('li')  
+  li.className = 'collection-item'  
+  li.appendChild(document.createTextNode(taskInput.value))  
+  const link = document.createElement('a')
+  link.className = 'delete-item secondary-content';
+  link.innerHTML = '<i class="fa fa-remove"></li>'  
+  li.appendChild(link)
+  taskList.appendChild(li)
+
+  //clear input
+  taskInput.value = ''  
+  
+}
+
+
+
+
